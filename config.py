c787a8c5c6c888701c44fe5287207df70ef3a879
kod-ou <kodouaccess@gmail.com>
2025-12-17 12:12:29 +0800

    Updated config.py

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ from supabase import create_client, Client
 
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-SUPABASE_URL = os.getenv('VITE_SUPABASE_URL')
-SUPABASE_KEY = os.getenv('VITE_SUPABASE_ANON_KEY')
+TELEGRAM_BOT_TOKEN = os.getenv('8119606757:AAEqkm1FyBu51bmeb4sS5PLj0dSoFZAv_nI')
+SUPABASE_URL = os.getenv('https://zetgffumucclwevnkfww.supabase.co')
+SUPABASE_KEY = os.getenv('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpldGdmZnVtdWNjbHdldm5rZnd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MzA5NzMsImV4cCI6MjA4MTUwNjk3M30.4C7qp_90KBnIstmzMYXY08BoNXj9-qMR-7cY6DJDvhk')
 
 if not TELEGRAM_BOT_TOKEN:
-    raise ValueError("TELEGRAM_BOT_TOKEN is not set in .env file")
+    raise ValueError("8119606757:AAEqkm1FyBu51bmeb4sS5PLj0dSoFZAv_nI")
 if not SUPABASE_URL:
-    raise ValueError("VITE_SUPABASE_URL is not set in .env file")
+    raise ValueError("https://zetgffumucclwevnkfww.supabase.co")
 if not SUPABASE_KEY:
-    raise ValueError("VITE_SUPABASE_ANON_KEY is not set in .env file")
+    raise ValueError("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpldGdmZnVtdWNjbHdldm5rZnd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MzA5NzMsImV4cCI6MjA4MTUwNjk3M30.4C7qp_90KBnIstmzMYXY08BoNXj9-qMR-7cY6DJDvhk")
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: Client = create_client(https://zetgffumucclwevnkfww.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpldGdmZnVtdWNjbHdldm5rZnd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MzA5NzMsImV4cCI6MjA4MTUwNjk3M30.4C7qp_90KBnIstmzMYXY08BoNXj9-qMR-7cY6DJDvhk)
